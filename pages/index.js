5fafebe8e07fffb7720384716682ac6d1b6f7517
Zedan Saheer <zed.saheer5@gmail.com>
2021-12-08 23:51:56 +0530

    setting up api call

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { Flex, Box, Text, Button } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Property } from '../components/Property'
 
-import { baseURL,fetchApi } from '../utils/fetchApi'
+import { fetchApi,baseUrl } from '../utils/fetchApi'
 
 export const Banner = ({ purpose , title,titleTwo,desc,descTwo , linkName , imageUrl , buttonText}) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
@@ -18,9 +19,9 @@ export const Banner = ({ purpose , title,titleTwo,desc,descTwo , linkName , imag
   </Flex>
 )
 
-export default function Home({propertyForRent,propertyForSale}) {
+export default function Home({propForRent,propForSale}) {
 
-  console.log(propertyForRent,propertyForSale);
+  console.log(propForRent,propForSale);
 
   return (
     <div>
@@ -33,6 +34,11 @@ export default function Home({propertyForRent,propertyForSale}) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110798997/d9446cee36ba4f839c8fedd0e0b52208"
       />
+      <Flex>
+        {propForRent.map((property)=>{
+          return <Property property={property} key={property.id}/>
+        })}
+      </Flex>
       <Banner purpose="Buy a home" 
         title="Find ,buy & own your"
         titleTwo="Dream home"
@@ -42,18 +48,23 @@ export default function Home({propertyForRent,propertyForSale}) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110798997/d9446cee36ba4f839c8fedd0e0b52208"
       />
+       <Flex>
+        {propForRent.map((property)=>{
+          return <Property property={property} key={property.id}/>
+        })}
+      </Flex>
     </div>
   )
 }
 
 export async function getStaticProps(){
-  const propertyForSale = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
-  const propertyForRent = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+  const propForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`)
+  const propForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`)
 
   return{
     props:{
-      propertyForSale:propertyForSale.hits,
-      propertyForRent:propertyForRent.hits,
+      propForRent:propForRent?.hits,
+      propForSale:propForSale?.hits,
     }
   }
 }
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const baseURL = 'hhtps://bayut.p.rapidapi.com'
-
+export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async (url) => {
     const {data} = await axios.get((url),{
@@ -10,5 +9,6 @@ export const fetchApi = async (url) => {
             'x-rapidapi-key': '328841f95amsh101368d51673ff2p1629dejsn1ae63cc7db1e'
         }
     });
-    return data
+
+    return data;
 }
